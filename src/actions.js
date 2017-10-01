d9643e52e9c7a02988d6032f06da4d06b674605f
VladimirJarabica <vladojarabica@gmail.com>
2017-10-01 11:43:34 +0200

    initial commit

---
+++ src/actions.js
@@ -0,0 +1,85 @@
+// @flow
+import type { Config, SaveResults } from 'splitster/src/main'
+
+type InitServer = {
+  type: 'splitster/INIT_SERVER',
+  payload: {
+    config: Config,
+    user: ?Object,
+    def: ?SaveResults,
+  },
+}
+type InitClient = {
+  type: 'splitster/INIT_CLIENT',
+  payload: {
+    config: Config,
+    user: ?Object,
+  },
+}
+type ServerToSave = {
+  type: 'splitster/SERVER_TO_SAVE',
+}
+type ClientToSave = {
+  type: 'splitster/CLIENT_TO_SAVE',
+}
+type Run = {
+  type: 'splitster/RUN',
+  payload: string,
+}
+type Set = {
+  type: 'splitster/SET',
+  payload: {
+    testId: string,
+    variantId: string,
+  },
+}
+
+export type Action =
+  | InitServer
+  | InitClient
+  | ServerToSave
+  | ClientToSave
+  | Run
+  | Set
+
+export const initServer = (
+  config: Config,
+  user?: Object,
+  def?: SaveResults,
+): InitServer => ({
+  type: 'splitster/INIT_SERVER',
+  payload: {
+    config,
+    user,
+    def,
+  },
+})
+
+export const initClient = (config: Config, user?: Object): InitClient => ({
+  type: 'splitster/INIT_CLIENT',
+  payload: {
+    config,
+    user,
+  },
+})
+
+export const serverToSave = (): ServerToSave => ({
+  type: 'splitster/SERVER_TO_SAVE',
+})
+
+export const clientToSave = (): ClientToSave => ({
+  type: 'splitster/CLIENT_TO_SAVE',
+})
+
+export const run = (test: string): Run => ({
+  type: 'splitster/RUN',
+  payload: test,
+})
+
+export const set = (testId: string, variantId: string): Set => ({
+  type: 'splitster/SET',
+  payload: {
+    testId,
+    variantId,
+  },
+})
---
+++ src/reducer.js
@@ -0,0 +1,51 @@
+// @flow
+// import splitsterInit, { server as serverInit } from 'splitster'
+import splitsterInit, {
+  server as serverInit,
+  SplitsterClient,
+  SplitsterServer,
+} from 'splitster'
+
+import type { SaveResults } from 'splitster/src/main'
+
+import type { Action } from './actions'
+
+/**
+ * Create server (serverInit)
+ * then change from server (server splitster)
+ * to save (SaveResults)
+ * and then to client (client splitster)
+ */
+const splitsterReducer = (
+  state: SplitsterClient | SaveResults | SplitsterServer = {},
+  action: Action,
+) => {
+  switch (action.type) {
+    case 'splitster/INIT_SERVER':
+      // $FlowFixMe
+      return serverInit(action.payload.config, action.payload.user, action.payload.def || state)
+    case 'splitster/INIT_CLIENT':
+      // $FlowFixMe
+      return splitsterInit(action.payload.config, action.payload.user, state)
+    case 'splitster/SERVER_TO_SAVE':
+    case 'splitster/CLIENT_TO_SAVE':
+      // $FlowFixMe
+      return state.getSaveResults()
+    case 'splitster/RUN':
+      if (action.test) {
+        // $FlowFixMe
+        state.run(action.payload)
+      } else {
+        // $FlowFixMe
+        state.runAll()
+      }
+      return state
+    case 'splitster/SET':
+      // $FlowFixMe
+      return state.set(action.payload.testId, action.payload.variantId)
+    default:
+      return state
+  }
+}
+
+export default splitsterReducer
